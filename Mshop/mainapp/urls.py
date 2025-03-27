bccf5187fd4a39f663de94d5c5f03d4ac6052490
maneesh55 <maneeshem842@gmail.com>
2025-03-27 23:10:09 +0530

    inside of product

--- Mshop/mainapp/urls.py
+++ Mshop/mainapp/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
+    # ('<urlpattern>',views.<ClassBased View>.as_view(),name= '<path_reference_name>')
     path('', views.homeView, name = 'homepage'),
     path('about', views.aboutView, name = 'aboutpage'),
-    path('contact', views.contactView, name= 'contactpage') 
+    path('contact', views.contactView, name= 'contactpage'),
+    path('products/<int:pk>', views.ProductDetails.as_view(), name = 'prod_details')
 ]
